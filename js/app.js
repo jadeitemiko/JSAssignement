4ba1723956bbd9f014142e4f1027978c94378cdd
GGL <gabi@live.se>
2025-10-01 10:19:26 +0200

    testar rader

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log ("HelloWorld");
+console.log ("This is line 1");
+console.log ("This is line 2");
+console.log ("This is line 13"); console.log ("This is line 14");
+console.log ("This is line 5");;
