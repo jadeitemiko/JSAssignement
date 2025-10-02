13cda353b836624d52cfed1289c37883a3a7d4c4
GGL <gabi@live.se>
2025-10-02 13:12:53 +0200

    laga lasagne

--- js/app-P9.js
+++ js/app-P9.js
@@ -1,3 +1,28 @@
-console.log("Hello World! Gustav är hungrig")
+//hur upprepar man mindre kod i if-statement? genom funktioner
 
-function makeLasagna(){}
+console.log("Do you want lasagna?")
+
+function makeLasagna(){
+  console.log("Yes, let's make lasagna!")
+  console.log("Fry meat.");
+  console.log("Cut onions.");
+  console.log("Fry onions.");
+  console.log("Add tomato sauce.");
+  console.log("Add spices.");
+  console.log("Boil.")
+}
+
+let isHungry = false;
+//breakfast
+if(isHungry){
+  makeLasagna();
+}
+else {
+  console.log("No thanks, not hungry.")
+}
+
+isHungry = true;
+//lunch
+if(isHungry){
+  makeLasagna();
+}
